e25dc82dc86b6f94fc0955a072bb43a8d601df43
bhavyatatavarthi <bhavyat1008@gmail.com>
2023-11-01 19:16:46 -0500

    completed

--- routes/board.js
+++ routes/board.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   console.log(`rows:${query.rows}`)
   console.log(`cols:${query.cols}`)
 
-  res.render('board', { title: 'Board Display',query:query });
+  res.render('board', { title: 'Board Display',query:query })
 });
 
 module.exports = router;
