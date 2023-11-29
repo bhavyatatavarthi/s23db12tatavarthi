e624e3e08377e7af54f0d621805c7aad4bd62a10
bhavyatatavarthi <bhavyat1008@gmail.com>
2023-11-28 23:32:39 -0600

    changes are made

--- models/birds.js
+++ models/birds.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
 
 const birdsSchema = mongoose.Schema({
-Name: String,
-  color: String,
-  Age: Number
+Name: {
+  type: String,
+    required: true,
+    trim: true, 
+    minlength: 1, 
+    maxlength: 255, 
+},
+  color:{
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    maxlength: 255,
+  } ,
+  Age: {
+    type:Number,
+    required:true,
+    min:1,
+    max:100
+  }
 });
 
 module.exports = mongoose.model("birds", birdsSchema);
--- routes/birds.js
+++ routes/birds.js
@@ -1,15 +1,24 @@
 var express = require('express');
 const birds_controllers= require('../controllers/birds');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 /* GET costumes */
 router.get('/', birds_controllers.birds_view_all_Page);
 /* GET detail costume page */
-router.get('/detail', birds_controllers.birds_view_one_Page);
+router.get('/detail',secured, birds_controllers.birds_view_one_Page);
 /* GET create costume page */
-router.get('/create', birds_controllers.birds_create_Page);
+router.get('/create',secured, birds_controllers.birds_create_Page);
 /* GET create update page */
-router.get('/update', birds_controllers.birds_update_Page);
+router.get('/update',secured, birds_controllers.birds_update_Page);
 /* GET delete costume page */
-router.get('/delete', birds_controllers.birds_delete_Page);
+router.get('/delete',secured, birds_controllers.birds_delete_Page);
 
 module.exports = router;
